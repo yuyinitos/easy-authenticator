67eea5e974ef0af60ba142161c3c8640be18b18f
yuyinitos <support@consumedia.nl>
2015-04-12 12:53:56 +0200

    Update socialAuthenticator.php

--- src/Config/socialAuthenticator.php
+++ src/Config/socialAuthenticator.php
@@ -29,7 +29,7 @@ return [
      * via email. This is useful for many reasons, such as to prevent
      * spam. If you set this value to TRUE, you will then also need
      * to set up mailing as per laravel.com/docs/5.0/mail.
-     * Once set up is done easyAuthenticator will take care of the rest.
+     * Once set up is done socialAuthenticator will take care of the rest.
      */
     'activation' => FALSE,
     // Set a subject line for the email.
