188184cc71e654b2a0d03467e074c91928b5c3f2
yuyinitos <support@consumedia.nl>
2015-04-12 12:42:25 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -11,25 +11,25 @@
 |
 */
 Route::controllers([
-    'auth' => 'Bernardino\EasyAuthenticator\AuthController',
-    'password' => 'Bernardino\EasyAuthenticator\PasswordController',
+    'auth' => 'Yuyinitos\SocialAuthenticator\AuthController',
+    'password' => 'Yuyinitos\SocialAuthenticator\PasswordController',
 ]);
 
-Route::get(config('easyAuthenticator.login_page'), function() {
-    return view('easyAuthenticator::login');
+Route::get(config('socialAuthenticator.login_page'), function() {
+    return view('socialAuthenticator::login');
 });
 
-Route::get(config('easyAuthenticator.logout'), function() {
+Route::get(config('socialAuthenticator.logout'), function() {
     return $this->app['authenticator']->logout();
 });
 
-Route::get(config('easyAuthenticator.login_redirect'), function() {
-    $user = Bernardino\EasyAuthenticator\Models\User::find(\Auth::id());
-    return view('easyAuthenticator::dashboard')->with('user', $user);
+Route::get(config('socialAuthenticator.login_redirect'), function() {
+    $user = Yuyinitos\SocialAuthenticator\Models\User::find(\Auth::id());
+    return view('socialAuthenticator::dashboard')->with('user', $user);
 });
 
-Route::get('easyAuth/{provider?}', function($provider = null) {
+Route::get('socialAuth/{provider?}', function($provider = null) {
     return $this->app['authenticator']->login($provider);
 });
 
-Route::get('activate/{code}', 'bernardino\EasyAuthenticator\AuthController@accountIsActive');
+Route::get('activate/{code}', 'Yuyinitos\SocialAuthenticator\AuthController@accountIsActive');
